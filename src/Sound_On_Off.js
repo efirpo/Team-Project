c0d57854c9b5fea695a808a5302e7ddf1d19d2b6
DJ Zevenbergen <djzevenbergen@gmail.com>
2020-04-28 22:16:59 -0700

    add new scary sound and ambient music. Also add jumping chair

--- src/Sound_On_Off.js
+++ src/Sound_On_Off.js
@@ -2,22 +2,50 @@ import "./sounds/intro_song.mp3"; import "./sounds/ambient_song.mp3"; import "./
 import "./sounds/scary_flashback.mp3";
 import $ from 'jquery';
 import * as THREE from 'three';
+import t from './main.js';
+import scene from './main.js';
+import setupScene from './main.js';
+import addObjectDynamically from "./main.js";
 
 let someFlag = false;
 let someFlag2 = false;
+let someFlag3 = false;
+let someFlag4 = false;
+
+
 
 export function soundChange(cam) {
+
+  if (cam.position.z > 700 && someFlag3 === false) {
+
+
+    someFlag3 = true;
+
+    var listener = new THREE.AudioListener();
+    var audioLoader = new THREE.AudioLoader();
+    cam.add(listener);
+
+    var sound = new THREE.Audio(listener);
+
+    audioLoader.load('./assets/sounds/ambient_song.mp3', function (buffer) {
+      sound.setBuffer(buffer);
+      sound.setLoop(false);
+      sound.setVolume(0.2);
+      sound.play();
+
+    });
+  }
 
   if (cam.position.x > -225 && cam.position.x < 225 && cam.position.z < 1250 && cam.position.z > 750) {
     if (someFlag === false) {
 
       someFlag = true;
 
-      var listener = new THREE.AudioListener();
-      var audioLoader = new THREE.AudioLoader();
+      listener = new THREE.AudioListener();
+      audioLoader = new THREE.AudioLoader();
       cam.add(listener);
 
-      var sound = new THREE.Audio(listener);
+      sound = new THREE.Audio(listener);
 
       audioLoader.load('./assets/sounds/scary_flashback.mp3', function (buffer) {
         sound.setBuffer(buffer);
@@ -38,7 +66,9 @@ export function soundChange(cam) {
 
       $("#credits p").text(`There is a thing here.`);
     }
-  } else if (cam.position.x > -2500 && cam.position.x < -1750 && cam.position.z < 500 && cam.position.z > -250) {
+  }
+
+  if (cam.position.x > -2500 && cam.position.x < -1750 && cam.position.z < 500 && cam.position.z > -250) {
     if (someFlag2 === false) {
       someFlag2 = true;
       listener = new THREE.AudioListener();
@@ -55,12 +85,33 @@ export function soundChange(cam) {
 
       });
     }
+  }
+
+  if (cam.position.x > -1495 && cam.position.x < -1006 && cam.position.z > -648 && cam.position.z < 492) {
+    if (someFlag4 === false) {
+      someFlag4 = true;
+      listener = new THREE.AudioListener();
+      cam.add(listener);
+
+      sound = new THREE.Audio(listener);
+      audioLoader = new THREE.AudioLoader();
+
+      audioLoader.load('./assets/sounds/panic_heartbeat.mp3', function (buffer) {
+        sound.setBuffer(buffer);
+        sound.setLoop(false);
+        sound.setVolume(0.2);
+        sound.play();
+
+      });
+
+      // addObjectDynamically(2);
 
 
-
+    }
   }
 
 
+  return someFlag4;
 }
 
 // export function soundOn(object, cam) {
